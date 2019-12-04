56473ae4d9e76b6683d31469f65700f5a0a3e859
maritlage2020 <maritlage2020.amana@gmail.com>
2019-12-04 15:47:20 +0900

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd4ed301f32b6310ed3599bc7a81ed3244f626580a5d94ed478a42632de133ddf31a4b5794afdd584e7f1f30d539a0315d9fa04909220ce0f210e428daa1f7d03'
